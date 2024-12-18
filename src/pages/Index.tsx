554ffe3f5e99619d0c20f05815df6e8099b28da6
brasetz <connectbrasetz@gmail.com>
2024-12-18 12:08:26 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,7 +13,7 @@ const Index = () => {
   const [showChart, setShowChart] = useState(false);
   const [showBuyModal, setShowBuyModal] = useState(false);
   const [showSellModal, setShowSellModal] = useState(false);
-  const COIN_VALUE = 0.035;
+  const COIN_VALUE = 0.04;
 
   const handleBuy = () => {
     setShowBuyModal(true);
